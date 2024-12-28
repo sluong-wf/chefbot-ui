815a69b1dc9064f4f6ffab81ca37ab46ac650d19
Shayla Luong <sluong910@gmail.com>
2024-12-28 09:33:15 -0800

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename='chefbot-ui'>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={"Created by Shayla"} />
+          <Route path="/about" element={<AboutPage />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
